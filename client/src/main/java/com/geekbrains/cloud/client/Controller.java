093bbb60cb3f55cd1afd385f2600ee13da44f010
ivanov-DM <dmitryi4141@gmail.ru>
2019-07-31 17:15:28 +0300

    Initial commit

---
+++ client/src/main/java/com/geekbrains/cloud/client/Controller.java
@@ -0,0 +1,10 @@
+package com.geekbrains.cloud.client;
+
+import com.geekbrains.cloud.common.Message;
+
+public class Controller {
+
+    public void doSomething() {
+        Message message = new Message("");
+    }
+}
